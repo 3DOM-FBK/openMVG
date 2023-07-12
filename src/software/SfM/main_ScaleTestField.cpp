205080fbee26b500abe9c2c8957e47d1276271bb
Roberto <rbattisti@fbk.eu>
2023-07-12 14:00:05 +0200

    GCP registration done twice in order to improve precision. Updated .gitignore

--- src/software/SfM/main_ScaleTestField.cpp
+++ src/software/SfM/main_ScaleTestField.cpp
@@ -39,172 +39,176 @@ void gcpRegister(SfM_Data & sfm_data)
   // - compute a coarse registration between the controls points & the triangulated point
   // - transform the scene according the found transformation
   //---
-  std::map<IndexT, Vec3> map_control_points, map_triangulated;
-  
-  std::map<IndexT, double> map_triangulation_errors;
-  for (const auto & control_point_it : sfm_data.control_points)
+
+  for (int repeat = 0; repeat < 2; repeat++)
   {
-    const Landmark & landmark = control_point_it.second;
-    //Triangulate the observations:
-    const Observations & obs = landmark.obs;
-
-    if (obs.size() < 2)
-    {
-        std::cout << "Control point must be defined in at least 2 pictures. Skipping.\n\n";
-        continue;
-    }
-
-    std::vector<Vec3> bearing;
-    std::vector<Mat34> poses;
-    bearing.reserve(obs.size());
-    poses.reserve(obs.size());
-
-    for (const auto & obs_it : obs)
-    {
-      const View * view = sfm_data.views.at(obs_it.first).get();
-      if (!sfm_data.IsPoseAndIntrinsicDefined(view))
-        continue;
-      const openMVG::cameras::IntrinsicBase * cam = sfm_data.GetIntrinsics().at(view->id_intrinsic).get();
-      const openMVG::geometry::Pose3 pose = sfm_data.GetPoseOrDie(view);
-      const Vec2 pt = obs_it.second.x;
-      bearing.emplace_back((*cam)(cam->get_ud_pixel(pt)));
-      poses.emplace_back(pose.asMatrix());
-    }
-
-    const Eigen::Map<const Mat3X> bearing_matrix(bearing[0].data(), 3, bearing.size());
+    std::map<IndexT, Vec3> map_control_points, map_triangulated;
     
-    Vec4 Xhomogeneous;
-    if (!TriangulateNViewAlgebraic(bearing_matrix, poses, &Xhomogeneous))
+    std::map<IndexT, double> map_triangulation_errors;
+    for (const auto & control_point_it : sfm_data.control_points)
     {
-      std::cout << "Invalid triangulation.\n\n";
-      return;
-    }
+      const Landmark & landmark = control_point_it.second;
+      //Triangulate the observations:
+      const Observations & obs = landmark.obs;
 
-    const Vec3 X = Xhomogeneous.hnormalized();
-    // Test validity of the hypothesis (front of the cameras):
-    bool bCheirality = true;
-    int i(0);
-    double reprojection_error_sum(0.0);
-    for (const auto & obs_it : obs)
-    {
-      const View * view = sfm_data.views.at(obs_it.first).get();
-      if (!sfm_data.IsPoseAndIntrinsicDefined(view))
-        continue;
-
-      const Pose3 pose = sfm_data.GetPoseOrDie(view);
-      bCheirality &= CheiralityTest(bearing[i], pose, X);
-      const openMVG::cameras::IntrinsicBase * cam = sfm_data.GetIntrinsics().at(view->id_intrinsic).get();
-      const Vec2 pt = obs_it.second.x;
-      const Vec2 residual = cam->residual(pose(X), pt);
-      reprojection_error_sum += residual.norm();
-      ++i;
-    }
-    if (bCheirality) // Keep the point only if it has a positive depth
-    {
-      map_triangulated[control_point_it.first] = X;
-      map_control_points[control_point_it.first] = landmark.X;
-      map_triangulation_errors[control_point_it.first] = reprojection_error_sum/(double)bearing.size();
-    }
-    else
-    {
-      std::cout << "Control Point cannot be triangulated (not in front of the cameras)" << std::endl;
-      return;
-    }
-  }
-
-  if (map_control_points.size() < 3)
-  {
-    std::cout << "Insufficient number of triangulated control points.\n\n";
-    return;
-  }
-
-  // compute the similarity
-  {
-    // data conversion to appropriate container
-    Mat x1(3, map_control_points.size()),
-        x2(3, map_control_points.size());
-    
-    IndexT id_col = 0;
-    for (const auto & cp : map_control_points)
-    {
-      x1.col(id_col) = map_triangulated[cp.first];
-      x2.col(id_col) = cp.second;
-      ++id_col;
-    }
-
-    std::cout
-      << "Control points observation triangulations:\n"
-      << x1 << std::endl << std::endl
-      << "Control points coords:\n"
-      << x2 << std::endl << std::endl;
-
-    Vec3 t;
-    Mat3 R;
-    double S;
-    if (openMVG::geometry::FindRTS(x1, x2, &S, &t, &R))
-    {
-      openMVG::geometry::Refine_RTS(x1,x2,&S,&t,&R);
-      std::cout << "Found transform:\n"
-        << " scale: " << S << "\n"
-        << " rotation:\n" << R << "\n"
-        << " translation: "<< t.transpose() << std::endl;
-
-      //--
-      // Apply the found transformation as a 3D Similarity transformation matrix // S * R * X + t
-      //--
-
-      const openMVG::geometry::Similarity3 sim(geometry::Pose3(R, -R.transpose() * t/S), S);
-      openMVG::sfm::ApplySimilarity(sim, sfm_data);
-
-      // Display some statistics:
-      std::stringstream os;
-      for (Landmarks::const_iterator iterL = sfm_data.control_points.begin();
-        iterL != sfm_data.control_points.end(); ++iterL)
+      if (obs.size() < 2)
       {
-        if (iterL->second.obs.size() < 2)
-        {
-            continue;
-        }
+          std::cout << "Control point must be defined in at least 2 pictures. Skipping.\n\n";
+          continue;
+      }
 
-        const IndexT CPIndex = iterL->first;
-        // If the control point has not been used, continue...
-        if (map_triangulation_errors.find(CPIndex) == map_triangulation_errors.end())
+      std::vector<Vec3> bearing;
+      std::vector<Mat34> poses;
+      bearing.reserve(obs.size());
+      poses.reserve(obs.size());
+
+      for (const auto & obs_it : obs)
+      {
+        const View * view = sfm_data.views.at(obs_it.first).get();
+        if (!sfm_data.IsPoseAndIntrinsicDefined(view))
+          continue;
+        const openMVG::cameras::IntrinsicBase * cam = sfm_data.GetIntrinsics().at(view->id_intrinsic).get();
+        const openMVG::geometry::Pose3 pose = sfm_data.GetPoseOrDie(view);
+        const Vec2 pt = obs_it.second.x;
+        bearing.emplace_back((*cam)(cam->get_ud_pixel(pt)));
+        poses.emplace_back(pose.asMatrix());
+      }
+
+      const Eigen::Map<const Mat3X> bearing_matrix(bearing[0].data(), 3, bearing.size());
+      
+      Vec4 Xhomogeneous;
+      if (!TriangulateNViewAlgebraic(bearing_matrix, poses, &Xhomogeneous))
+      {
+        std::cout << "Invalid triangulation.\n\n";
+        return;
+      }
+
+      const Vec3 X = Xhomogeneous.hnormalized();
+      // Test validity of the hypothesis (front of the cameras):
+      bool bCheirality = true;
+      int i(0);
+      double reprojection_error_sum(0.0);
+      for (const auto & obs_it : obs)
+      {
+        const View * view = sfm_data.views.at(obs_it.first).get();
+        if (!sfm_data.IsPoseAndIntrinsicDefined(view))
           continue;
 
-        os
-          << "CP index: " << CPIndex << "\n"
-          << "CP triangulation error: " << map_triangulation_errors[CPIndex] << " pixel(s)\n"
-          << "CP registration error: "
-          << (sim(map_triangulated[CPIndex]) - map_control_points[CPIndex]).norm() << " user unit(s)"<< "\n\n";
+        const Pose3 pose = sfm_data.GetPoseOrDie(view);
+        bCheirality &= CheiralityTest(bearing[i], pose, X);
+        const openMVG::cameras::IntrinsicBase * cam = sfm_data.GetIntrinsics().at(view->id_intrinsic).get();
+        const Vec2 pt = obs_it.second.x;
+        const Vec2 residual = cam->residual(pose(X), pt);
+        reprojection_error_sum += residual.norm();
+        ++i;
       }
-      std::cout << os.str();
+      if (bCheirality) // Keep the point only if it has a positive depth
+      {
+        map_triangulated[control_point_it.first] = X;
+        map_control_points[control_point_it.first] = landmark.X;
+        map_triangulation_errors[control_point_it.first] = reprojection_error_sum/(double)bearing.size();
+      }
+      else
+      {
+        std::cout << "Control Point cannot be triangulated (not in front of the cameras)" << std::endl;
+        return;
+      }
     }
-    else
-    {
-      std::cout << "Registration failed. Please check your Control Points coordinates.\n\n";
-    }
-  }
 
-  //---
-  // Bundle adjustment with GCP
-  //---
-  {
-    using namespace openMVG::sfm;
-    Bundle_Adjustment_Ceres::BA_Ceres_options options;
-    Bundle_Adjustment_Ceres bundle_adjustment_obj(options);
-    Control_Point_Parameter control_point_opt(20.0, true);
-    if (!bundle_adjustment_obj.Adjust(sfm_data,
-        Optimize_Options
-        (
-          cameras::Intrinsic_Parameter_Type::NONE, // Keep intrinsic constant
-          Extrinsic_Parameter_Type::ADJUST_ALL, // Adjust camera motion
-          Structure_Parameter_Type::ADJUST_ALL, // Adjust structure
-          control_point_opt // Use GCP and weight more their observation residuals
+    if (map_control_points.size() < 3)
+    {
+      std::cout << "Insufficient number of triangulated control points.\n\n";
+      return;
+    }
+
+    // compute the similarity
+    {
+      // data conversion to appropriate container
+      Mat x1(3, map_control_points.size()),
+          x2(3, map_control_points.size());
+      
+      IndexT id_col = 0;
+      for (const auto & cp : map_control_points)
+      {
+        x1.col(id_col) = map_triangulated[cp.first];
+        x2.col(id_col) = cp.second;
+        ++id_col;
+      }
+
+      std::cout
+        << "Control points observation triangulations:\n"
+        << x1 << std::endl << std::endl
+        << "Control points coords:\n"
+        << x2 << std::endl << std::endl;
+
+      Vec3 t;
+      Mat3 R;
+      double S;
+      if (openMVG::geometry::FindRTS(x1, x2, &S, &t, &R))
+      {
+        openMVG::geometry::Refine_RTS(x1,x2,&S,&t,&R);
+        std::cout << "Found transform:\n"
+          << " scale: " << S << "\n"
+          << " rotation:\n" << R << "\n"
+          << " translation: "<< t.transpose() << std::endl;
+
+        //--
+        // Apply the found transformation as a 3D Similarity transformation matrix // S * R * X + t
+        //--
+
+        const openMVG::geometry::Similarity3 sim(geometry::Pose3(R, -R.transpose() * t/S), S);
+        openMVG::sfm::ApplySimilarity(sim, sfm_data);
+
+        // Display some statistics:
+        std::stringstream os;
+        for (Landmarks::const_iterator iterL = sfm_data.control_points.begin();
+          iterL != sfm_data.control_points.end(); ++iterL)
+        {
+          if (iterL->second.obs.size() < 2)
+          {
+              continue;
+          }
+
+          const IndexT CPIndex = iterL->first;
+          // If the control point has not been used, continue...
+          if (map_triangulation_errors.find(CPIndex) == map_triangulation_errors.end())
+            continue;
+
+          os
+            << "CP index: " << CPIndex << "\n"
+            << "CP triangulation error: " << map_triangulation_errors[CPIndex] << " pixel(s)\n"
+            << "CP registration error: "
+            << (sim(map_triangulated[CPIndex]) - map_control_points[CPIndex]).norm() << " user unit(s)"<< "\n\n";
+        }
+        std::cout << os.str();
+      }
+      else
+      {
+        std::cout << "Registration failed. Please check your Control Points coordinates.\n\n";
+      }
+    }
+
+    //---
+    // Bundle adjustment with GCP
+    //---
+    {
+      using namespace openMVG::sfm;
+      Bundle_Adjustment_Ceres::BA_Ceres_options options;
+      Bundle_Adjustment_Ceres bundle_adjustment_obj(options);
+      Control_Point_Parameter control_point_opt(20.0, true);
+      if (!bundle_adjustment_obj.Adjust(sfm_data,
+          Optimize_Options
+          (
+            cameras::Intrinsic_Parameter_Type::NONE, // Keep intrinsic constant
+            Extrinsic_Parameter_Type::ADJUST_ALL, // Adjust camera motion
+            Structure_Parameter_Type::ADJUST_ALL, // Adjust structure
+            control_point_opt // Use GCP and weight more their observation residuals
+            )
           )
         )
-      )
-    {
-     std::cout << "BA with GCP failed." << std::endl;
+      {
+      std::cout << "BA with GCP failed." << std::endl;
+      }
     }
   }
 }
@@ -239,7 +243,7 @@ bool readMarkersPositions(const std::string & sFileName, Landmarks & landmarks)
             continue;
         }
 
-        int markerId = std::stoi(vec_str[0]);
+        int markerId = std::stoi(vec_str[0]); // marker ID must be the exact Aruco Marker ID of the specified dictionary
 
         float x = std::stof(vec_str[1]);
         float y = std::stof(vec_str[2]);
@@ -368,7 +372,7 @@ int main(int argc, char **argv)
     //-------------------------------------------------------------------------
     // d. Triangulate position of unknown markers and export them in a txt file
     //-------------------------------------------------------------------------
-    dictionary = cv::aruco::getPredefinedDictionary(cv::aruco::DICT_6X6_250);
+    dictionary = cv::aruco::getPredefinedDictionary(cv::aruco::DICT_7X7_250);
 
     markerIds.clear();
     markerCorners.clear();
